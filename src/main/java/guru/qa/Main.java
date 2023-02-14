222f209f79533282e3387d1041a65e6eddfa0437
Dmitrii Tuchs <dtuchs@gmail.com>
2023-02-15 00:19:41 +0600

    Update Main.java

--- src/main/java/guru/qa/Main.java
+++ src/main/java/guru/qa/Main.java
@@ -1,7 +1,10 @@
 package guru.qa;
 
 public class Main {
+  
+  private static String msg = ""Hello qa.guru!!!!!!"";
+  
   public static void main(String[] args) {
-    System.out.println("Hello qa.guru!!!!!!");
+    System.out.println(msg);
   }
 }
